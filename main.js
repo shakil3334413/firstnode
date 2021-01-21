ed2b118fef8c141fa5c9de11fae938f4a6ae8ee4
shakil3334413 <shakil3334413@gmail.com>
2021-01-21 21:37:52 +0600

    paremeter bag

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 var http = require('http');
-
+var URL = require('url');
 var server = http.createServer(function(req, res) {
 
-    if (req.url == "/") {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<h3>This is home page</h3>');
-        res.end();
-    } else if (req.url == "/about") {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<h3>This is About page</h3>');
-        res.end();
-    } else if (req.url == "/contact") {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<h3>This is Contact page</h3>');
-        res.end();
-    }
+
+    var myurl = "https://www.facebook.com/blog.html?year=2020&month=2";
+    var urlpart = URL.parse(myurl, true);
+    var hostname = urlpart.host;
+    var pathname = urlpart.pathname;
+    var query = urlpart.search;
+    res.write(pathname);
+    res.end();
+
+
+    // if (req.url == "/") {
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.write('<h3>This is home page</h3>');
+    //     res.end();
+    // } else if (req.url == "/about") {
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.write('<h3>This is About page</h3>');
+    //     res.end();
+    // } else if (req.url == "/contact") {
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.write('<h3>This is Contact page</h3>');
+    //     res.end();
+    // }
+
+
 })
 
 
